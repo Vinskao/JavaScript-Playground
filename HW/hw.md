a59de8ae04a4d465817b75cb9563a415f0286e0e
Vinskao <tianyikao@gmail.com>
2023-11-08 17:51:09 +0800

    99

---
+++ HW/hw.md
@@ -0,0 +1,3 @@
+1. 使用 Table 印出九九乘法表
+2. 製作表單包含下列欄位:
+   ![Alt text](image.png)
---
+++ Lesson1/script.js
@@ -0,0 +1,35 @@
+document.write("hahaha<br>ccc");
+
+console.log("hahaha\nccc");
+
+var object = document.getElementById("add");
+object.innerHTML = "<h2>whoops</h2>";
+
+const FUN = 1;
+
+function test() {
+  if (true) {
+    let type = 3;
+    var mmmm = 44;
+    type = 9;
+    console.log(type);
+    console.log(mmmm);
+    console.log(type);
+  }
+}
+dog = encodeURI("大狗");
+console.log(dog);
+
+var mmmm = 445;
+console.log(mmmm + 'dddddd"ccsacs"\'');
+
+let type = 4;
+console.log(type);
+var mmmm = `hihihihih herherh  hrehrehre ${type}`;
+console.log(mmmm);
+
+console.log(FUN);
+
+var x = true;
+
+console.log(`c\\print${x + 2}`);
